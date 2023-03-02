d3881f4949059e8dda886cc3e9c3e1a1540f06ca
Antonio Novaes <antonionovaesjr@gmail.com>
2023-03-02 10:03:57 -0300

    add random and format code

--- example/main.tf
+++ example/main.tf
@@ -4,25 +4,34 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source  = "git::https://github.com/antonionovaesjr/terraform-ec2-module-example.git?ref=9977afcce8fc858222d3287addf6345a98f5d07d"
-  name               = "SERVER01"
-  instance_os        = "ubuntu"
-  instance_type      = "t3a.medium"
-  key_name           = "demonstracao-aws"
-  security_group_ids = ["sg-0030ec31e103d2104"]
-  subnet_id          = "subnet-94706afc"
-  volume_size        = 20
+  source              = "git::https://github.com/antonionovaesjr/terraform-module-aws-ec2.git?ref=v0.1.0-alpha"
+  name                = "SERVER01"
+  instance_os         = "ubuntu"
+  instance_type       = "t3a.medium"
+  key_name            = "demonstracao-aws"
+  security_group_ids  = ["sg-0030ec31e103d2104"]
+  subnet_id           = "subnet-94706afc"
+  volume_size         = 20
   user_data_file_path = "./user-data.sh"
-  enviroment = "Dev"
+  enviroment          = "Dev"
+}
+
+
+resource "random_string" "random_id" {
+  length      = 16
+  upper       = false
+  special     = false
+  min_numeric = 6
 }
 
 resource "aws_s3_bucket" "bucket_example" {
-  bucket = "my-123abc-example-bucket"
+  bucket = "my-example-bucket-${random_string.random_id.result}"
   tags = {
     Name        = "my-123abc-example-bucket"
     Environment = "Dev"
     Type        = "storage"
     Managed     = "IAC"
+    BuceketId   = random_string.random_id.result
   }
 }
 
